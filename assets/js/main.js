84c22060cbb82c294d6ff2898d6e1ef6a3eccf8e
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-06-23 09:50:57 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /*====== MENU SHOW =====*/
 // Validate if constant exist
@@ -37,28 +37,28 @@ function scrollHeader() {
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*=============== SHOW SCROLL UP ===============*/ 
-function scrollUp(){
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
     // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    if (this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
@@ -73,12 +73,17 @@ const sr = ScrollReveal({
     // reset: true
 })
 
-sr.reveal(`.home__header, .section__title`,{delay: 600})
-sr.reveal(`.home__footer`,{delay: 700})
-sr.reveal(`.home__img`,{delay: 900, origin: 'top'})
+sr.reveal(`.home__header, .section__title`, { delay: 600 })
+sr.reveal(`.home__footer`, { delay: 700 })
+sr.reveal(`.home__img`, { delay: 900, origin: 'top' })
 
-sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`,{origin: 'top', interval: 100})
-sr.reveal(`.specs__data, .discount__animate`,{origin: 'left', interval: 100})
-sr.reveal(`.specs__img, .discount__img`,{origin: 'right'})
-sr.reveal(`.case__img`,{origin: 'top'})
+sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`, { origin: 'top', interval: 100 })
+sr.reveal(`.specs__data, .discount__animate`, { origin: 'left', interval: 100 })
+sr.reveal(`.specs__img, .discount__img`, { origin: 'right' })
+sr.reveal(`.case__img`, { origin: 'top' })
 sr.reveal(`.case__data`)
+
+// copyright date
+const date = document.querySelector("#date");
+// set year
+date.innerHTML = new Date().getFullYear();
